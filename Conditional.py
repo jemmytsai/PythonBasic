40cd2d549bd14f377c901236092f108373e6192b
jemmy tsai <jemmytsai2020@gmail.com>
2021-03-03 02:15:33 +0000

    main.py 決定要跑那一個程式 BasicDigital.py 基礎數字的語法 BasicText.py 基礎文字語法 Conditional.py 比較運算子與邏輯運算子

--- Conditional.py
+++ Conditional.py
@@ -1,5 +1,6 @@
 # 比較運算子與邏輯運算子
-print('# 比較運算子與邏輯運算子','\n')
+print('--- 比較運算子與邏輯運算子 START ---\n')
+print('# 比較運算子','\n')
 
 # 1. Boolean(是/否) True , False(第一個字一定要大寫)  
 # 2. 數字的比對、字串的比對、
@@ -15,8 +16,66 @@ print("# 變數 b = 2 == 2 ， boolean值為:", b)
 #大寫和小寫是不一樣的
 print("# 大寫和小寫(jemmy Jemmy)是不一樣的:", "jemmy" == "Jemmy")
 
-print("# 使用 in 來看Ken word 是不是在字串中:", "good" in "good morning")
+print("# 使用 in 來看Key word 是不是在字串中:", "good" in "good morning")
 
 # and or 的boolean
 print("# 專門結合運算(True or False):", True or False)
-print("# 專門結合運算(5 > 3 - 1 and True):", 5 > 3 - 1 and True)
+print("# 專門結合運算(5 > 3 - 1 and True):", 5 > 3 - 1 and True,'\n')
+
+print("# 邏輯運算子\n")
+print("1. 單純的 if 判斷:\nif condition:\n\tstatement\n\t")
+print("2. if else 判斷:\nif condition:\n\tstatement1 for True condition\nelse:\n\tstatement2 for False condition\n")
+print("3. if-elif-elif-else 判斷:\nif condition1:\n\tstatement1 for True condition\nelif condition2 :\n\tstatement2 for True Condition2\nelif condition3 :\n\tstatement3 for True Condition3\nelse:\n\tstatements for Each Condition False")
+
+print("\n===== 中間線 =====\n")
+print('\nEXAMPLE: 使用input函式判斷輸入的值是否有超過60分\n')
+#加入輸入方式
+#float(轉成小數)
+newscore = float(input("請輸入成績看是有超過60(可以整數也可以有小數): "))
+if newscore >= 60:
+    print("PASS")
+else:
+    print("FAIL\n")
+
+print("\n===== 中間線 =====\n")
+
+# 巢狀條件判斷語法的說明與實作
+print('\nEXAMPLE: 巢狀條件判斷語法的說明與實作\n')
+newscore = float(input("請輸入成績(60以上及格、以下不及格、A:高於90、B:80~80、C:70~80、D:70以下):"))
+if newscore >= 60:
+    print("PASS")
+else:
+    print("FAIL")
+
+if newscore >= 90:
+    print("RANK A")
+elif newscore >= 80:
+    print("Rank B")
+elif newscore >= 70:
+    print("RANK C")
+else:
+    print("RANK D")
+
+
+
+# 1. 單純的 if 判斷
+# if condition:
+#    statement
+
+# 2. if else 判斷
+# if condition:
+#     statement1 for True condition
+# else:
+#     statement2 for False condition
+
+# 3. if-elif-else 判斷
+# if condition1:
+#     statement1 for True Condition1
+# elif condition2 :
+#     statement2 for True Condition2
+# elif condition3 :
+#     statement3 for True Condition3
+# else:
+#     statements for Each Condition False
+
+print('--- 比較運算子與邏輯運算子 END ---\n')
--- main.py
+++ main.py
@@ -1,31 +1,43 @@
-# 此為Python基礎語法主要進入口，將所有的練習語法方煩至各個PY中
+# 此為Python基礎語法主要進入口，將所有的練習語法方式至各個PY中
 
+import sys
+
+# print ("查看Python版本", sys . version_info)
+# print ("查看Python版本", sys . version)
+
+# PYTHON 網路資源
+# PEP(Python Enhancement Proposals)) https://legacy.python.org/dev/peps/
 
 # Python 的一些基本知識語法
 # 加上#表示註解，所以這行為註解，程式並不會去執行，註解寫的愈好愈仔細，日後的維護會愈輕鬆
 # print("\n") 
 # \n : 換一行   \t : TAB   \b : backspace 指標往前一格
-# TODO:
+# TODO: 表示待做事項
 
 
 # TODO:寫一個 input 可輸入值，然後用if else 去判斷要跑以下那一個 import 語法
 
-learnNumber = int(input("請選擇課程 [1]基礎語法 [2]boolean和條件判斷語法 :"))
+LearnNumber = int(input("請選擇課程:\n[0]Python課程說明\n[1]基礎文字語法\n[2]基礎數字語法\n[3]boolean和條件判斷語法\n請輸入課程數字代碼:"))
 print("\n")
-if learnNumber == 1:
-  print('--- \nPython基礎語法 start ---','\n')
-  import basic #引用模組
-  print('--- Python基礎語法 end ---','\n')
-elif learnNumber == 2:
-  print('--- Python boolean和條件判斷語法 start ---','\n')
-  import Conditional #引用模組
-  print('--- Python boolean和條件判斷語法 end ---','\n')
-elif learnNumber == 3:
+if LearnNumber == 0:
+  print('--- Python ---','\n')
+elif LearnNumber == 1:
+  import BasicText  # Python 基礎數字語法
+elif LearnNumber == 2:
+  import BasicDigital # Python 基礎數字語法
+elif LearnNumber == 3:
+  import Conditional # Python boolean和條件判斷語法
+elif LearnNumber == 4:
   print("敬請期待，請隨時關注哦!!!")
 else:
   print("請輸入正確的課程代碼")
 
 
+#print('[1]基礎語法')
+#print('1. 加、減、乘、除\n2.文字的運用','\n')
+
+#print('[2]基礎語法')
+#print('1.boolean\n2.條件判斷語法','\n')
 
 #print("Drag out the arrow on the left to show the filetree")
 #from functions import greeting
